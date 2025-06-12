9c881aebe30924b3126db0a3f106becef4d2bfdc
ChunyoungTech <141386109+ChunyoungTech@users.noreply.github.com>
2025-06-12 16:56:12 +0800

    Add Historian live query page

---
+++ INX_WorkChecking/_idb/HistorianLive.aspx.cs
@@ -0,0 +1,42 @@
+using cyc.Page;
+using System;
+using System.Data;
+using System.Linq;
+using System.Web.UI.WebControls;
+
+namespace WebApp._idb
+{
+    public partial class HistorianLive : BasePageGrid
+    {
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            if (!IsPostBack)
+            {
+                ddlFactory.DataSource = dDB.QueryList<string>("select FacName from IDBFacData order by SeqID");
+                ddlFactory.DataBind();
+                ddlFactory.Items.Insert(0, "");
+            }
+        }
+
+        protected override void QueryCheck(int idx)
+        {
+        }
+
+        protected override DataTable QuerySourceData(int idx)
+        {
+            if (string.IsNullOrEmpty(ddlFactory.SelectedValue)) return null;
+            string table = ddlFactory.SelectedValue.Replace("FAB", "FAC") + "_Historian_Live";
+            bPara.Command = $@"select [DateTime],[TagName],[Value],[Quality] from FAC_Loader.dbo.[{table}] where 1=1
+{(string.IsNullOrWhiteSpace(txtTagName.Text) ? string.Empty : " and TagName like @Tag")}
+{(string.IsNullOrWhiteSpace(txtQuality.Text) ? string.Empty : " and Quality=@Quality")}";
+            var param = new { Tag = $"%{txtTagName.Text.Trim()}%", Quality = txtQuality.Text };
+            return dDB.QueryDataTable(bPara.Command, param);
+        }
+
+        protected override GridPageOption SetPageSetting()
+        {
+            return new GridPageOption { GridOption = new[] { new GridOption { Grid = GridView1, Pager = ucPager, Query = btnQuery, AutoBind = true } } };
+        }
+    }
+}
